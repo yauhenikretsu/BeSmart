13ea62ea5c1809e3e45a937b9c0a03df21d2ffd5
yauhenikretsu <yauheni.kretsu@gmail.com>
2019-11-09 02:56:54 +0300

    new functions

--- sandbox/src/main/java/qa/base/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/qa/base/sandbox/MyFirstProgram.java
@@ -3,9 +3,25 @@ package qa.base.sandbox;
 public class MyFirstProgram {
 	
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		hello("user");
 
+		double a= 4;
+		double b =3;
+		System.out.println("Area of the surface = " + area1(a,b) + "mm");
 
+		double c=2;
+		System.out.println("Area of the kubik ="+ area2(c)+ "mm");
+	}
+	public static void hello(String somebody) {
+
+		System.out.println("Hello " + somebody + "!");
 	}
 
+	public static double area1(double a1, double b1){
+		return b1*a1;
+	}
+
+	public static double area2(double c1){
+		return c1*c1;
+	}
 }
